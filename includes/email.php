47fcbcd2014abfe8bbdd8da75c22513e7c955ba5
cferdinandi <chris@gomakethings.com>
2016-12-20 17:43:36 -0500

    v1.0.0

---
+++ includes/email.php
@@ -0,0 +1,24 @@
+<?php
+
+
+	function gmt_edd_for_wpwa_update_email( $user_id, $old_email ) {
+
+		if ( !class_exists( 'EDD_Customer' ) ) return;
+
+		// Get email data from EDD and user in database
+		$customer = new EDD_Customer( $old_email );
+		$user = get_user_by( 'id', $user_id );
+
+		// Make sure customer is found
+		if ( $customer->ID === 0 ) return;
+
+		// If new email isn't listed in customer profile, add it
+		if ( $customer->email !== $user->user_email && ( !is_array( $customer->emails ) && !in_array( $user->user_email, $customer->emails ) ) ) {
+			$customer->add_email( $user->user_email );
+		};
+
+		// Update customer email
+		$customer->set_primary_email( $user->user_email );
+
+	}
+	add_action( 'wpwebapp_after_email_change', 'gmt_edd_for_wpwa_update_email', 10, 2 );
